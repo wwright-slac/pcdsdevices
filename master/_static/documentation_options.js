a1667549350772257730a7001dd09c666275da60
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2021-09-16 22:34:54 +0000

    Update docs after building Travis build 2134 of pcdshub/pcdsdevices
    The docs were built from the tag 'master' against the commit
    1d42b563b6fca117d7efd71e8cc5d7b53f9fad69.
    
    The Travis build that generated this commit is at
    https://app.travis-ci.com/pcdshub/pcdsdevices/jobs/537929618.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.8.7/bin/doctr deploy --build-tags master --deploy-branch-name gh-pages --command '"doctr-versions-menu"'

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.7.1+98.g9de59d1',
+    VERSION: '4.7.1+114.g1d42b56',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
